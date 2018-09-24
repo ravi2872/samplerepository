9db95b576b2699d5239973507cc83f517832885b
Chandrika-PC\Chandrika <ravikumar.asvs@gmail.com>
2018-09-25 00:00:04 +0530

    Sample Commit

---
+++ mysampleproject/src/main/java/mysample/mysampleproject/App.java
@@ -0,0 +1,13 @@
+package mysample.mysampleproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
